9c163dc46584139234d9d7970a5fa54bd2a27edf
Seenivasan <vnseeni1999@gmail.com>
2025-03-16 11:41:55 +0530

    Selection sort logic changed on 16/03/2025

--- src/mainclass/SelectionSort.java
+++ src/mainclass/SelectionSort.java
@@ -6,11 +6,10 @@ public class SelectionSort {
 
 	public static void main(String[] args) {
 
-		int nums[] = { 5, 7, 8, 2, 4, 9 };// {5,2,4,7,8,9}
+		int nums[] = { 5, 7, 8, 2, 4, 9, 99, 6 , 8 };// {5,2,4,7,8,9}
 
-		int index = 0;
-
-		for (int k = nums.length; k > 1; k--) {
+		for (int k = nums.length; k > 0; k--) {
+			int index = 0;
 			int max = nums[0];
 			for (int i = 1; i < k; i++) {
 				if (max < nums[i]) {
@@ -19,7 +18,7 @@ public class SelectionSort {
 				}
 			}
 			int temp = nums[k - 1];
-			nums[k - 1] = max;
+			nums[k - 1] = nums[index];
 			nums[index] = temp;
 		}
 
