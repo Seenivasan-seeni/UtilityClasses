0995b0e60cfa001fd8ef212f796f17f0082d3ee6
Seenivasan <vnseeni1999@gmail.com>
2025-03-16 12:51:08 +0530

    Insertion Sort logic changed

--- src/mainclass/InsertionSort.java
+++ src/mainclass/InsertionSort.java
@@ -6,21 +6,18 @@ public class InsertionSort {
 
 	public static void main(String[] args) {
 
-		int num[] = { 6, 4, 7, 2, 9, 10, 3 ,99, 100};// 4,6,7,2,9,10,3
+		int num[] = { 6, 4, 7, 2, 9, 10, 3, 99, 100 };// 4,6,7,2,9,10,3
 
 		for (int i = 1; i < num.length; i++) {
 			int temp = num[i];
-			int newIndex = i;
-			int index = -1;
+			int numShiftIndex = i;
 			for (int j = i - 1; j >= 0; j--) {
 				if (num[j] > temp) {
-					num[newIndex] = num[j];
-					index = j;
-					newIndex--;
+					num[numShiftIndex] = num[j];
+					numShiftIndex--;
 				}
 			}
-			if(index != -1)
-				num[index] = temp;
+			num[numShiftIndex] = temp;
 		}
 		IntStream.of(num).forEach(nums -> System.out.print(nums + ", "));
 	}
