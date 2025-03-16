e6f2a6446ff78d13bb45663815c749dd61f1dc3f
Seenivasan <vnseeni1999@gmail.com>
2025-03-16 12:43:47 +0530

    Insertionsort java file committed

---
+++ src/mainclass/InsertionSort.java
@@ -0,0 +1,28 @@
+package mainclass;
+
+import java.util.stream.IntStream;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+
+		int num[] = { 6, 4, 7, 2, 9, 10, 3 ,99, 100};// 4,6,7,2,9,10,3
+
+		for (int i = 1; i < num.length; i++) {
+			int temp = num[i];
+			int newIndex = i;
+			int index = -1;
+			for (int j = i - 1; j >= 0; j--) {
+				if (num[j] > temp) {
+					num[newIndex] = num[j];
+					index = j;
+					newIndex--;
+				}
+			}
+			if(index != -1)
+				num[index] = temp;
+		}
+		IntStream.of(num).forEach(nums -> System.out.print(nums + ", "));
+	}
+
+}
