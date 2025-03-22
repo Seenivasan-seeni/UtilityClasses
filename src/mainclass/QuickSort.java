633069c760d63ea6b76613c29080c475a9aaced1
Seenivasan <vnseeni1999@gmail.com>
2025-03-22 17:26:51 +0530

    QuickSort logic corrected

--- src/mainclass/QuickSort.java
+++ src/mainclass/QuickSort.java
@@ -4,48 +4,6 @@ import java.util.stream.IntStream;
 
 public class QuickSort {
 
-//	public static void main(String[] args) {
-//		int[] arr = { 3, 7, 8, 2, 1, 10, 5, 100 };
-//		quickSort(arr, 0, arr.length - 1);
-//	}
-//
-//	public static void quickSort(int[] arr, int low, int high) {
-//		if (low < high) {
-//			// Find partition index
-//			int pi = partition(arr, low, high);
-//
-//			// Recursively sort elements before and after partition
-//			quickSort(arr, low, pi - 1);
-//			quickSort(arr, pi + 1, high);
-//		}
-//	}
-//
-//	// Partition function to place pivot element at correct position
-//	public static int partition(int[] arr, int low, int high) {
-//		int pivot = arr[high];
-//		int i = (low - 1);
-//
-//		for (int j = low; j < high; j++) {
-//			if (arr[j] <= pivot) {
-//				i++;
-//				// Swap arr[i] and arr[j]
-//				int temp = arr[i];
-//				arr[i] = arr[j];
-//				arr[j] = temp;
-//			}
-//		}
-//
-//		// Swap arr[i + 1] and arr[high] (pivot)
-//		int temp = arr[i + 1];
-//		arr[i + 1] = arr[high];
-//		arr[high] = temp;
-//
-//		IntStream.of(arr).forEach(num -> System.out.print(num + ", "));
-//		System.out.println();
-//
-//		return i + 1;
-//	}
-
 	public static void main(String[] args) {
 		int nums[] = { 3, 7, 8, 2, 1, 10, 5, 100 };// 3,2,1,5,7,8,10
 
@@ -57,14 +15,14 @@ public class QuickSort {
 	}
 
 	private static void sort(int[] nums, int low, int high) { // [3,2,1] ,0 ,2
-		if (low > high) {
+		if (low >= high) {
 			return;
 		}
-
+		int pivot = nums[high];
 		int mtp = low - 1;
 
 		for (int j = low; j < high; j++) {
-			if (nums[j] < nums[high]) {
+			if (nums[j] <= pivot) {
 				mtp++;
 				int temp = nums[mtp];
 				nums[mtp] = nums[j];
