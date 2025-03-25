a397fedbd55f23fbe63de69a232f722ddec6e305
Seenivasan <vnseeni1999@gmail.com>
2025-03-25 20:50:48 +0530

    Comments added

--- src/dsa/ReentrantLockMain.java
+++ src/dsa/ReentrantLockMain.java
@@ -7,22 +7,22 @@ class ReentrantLockEg {
 	ReentrantLock lock = new ReentrantLock();
 
 	void perform() {
-		lock.lock();
+		lock.lock(); // 1st lock
 		try {
 			System.out.println("Lock is acquired for the Thread - " + Thread.currentThread().getName());
 			callNextMethod();
 		} finally {
-			lock.unlock();
+			lock.unlock(); // Here, the counter value becomes 0 and next thread will start to acquire the thread
 			System.out.println("Lock is released for the Thread - " + Thread.currentThread().getName());
 		}
 	}
 
 	private void callNextMethod() {
-		lock.lock();
+		lock.lock();// 2nd lock for the same thread. It will increase only the counter value in the reentrant lock.
 		try {
 			System.out.println("Next method is called by using the same lock - " + Thread.currentThread().getName());
 		} finally {
-			lock.unlock();
+			lock.unlock(); // one thread lock is released. So the counter value will be decreased to 1.
 		}
 
 	}
